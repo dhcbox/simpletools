de725d258ab98b584ea3a048f00331db04a1eda8
dhc <dhc@192.168.0.107>
2017-02-04 23:55:44 +0800

    fileupload jar

--- test/com/dbutils/TxQueryRunnerTest.java
+++ test/com/dbutils/TxQueryRunnerTest.java
@@ -2,9 +2,11 @@ package com.dbutils;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.apache.commons.dbutils.handlers.MapListHandler;
 import org.junit.Test;
 
 import com.entity.Classes;
@@ -19,6 +21,17 @@ public class TxQueryRunnerTest {
 			List<Classes> list = qr.query("select * from classes", new BeanListHandler<>(Classes.class));
 			System.out.println(list);
 		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testQuery2(){
+		try {
+			QueryRunner qr = new TxQueryRunner();
+			List<Map<String, Object>> list = qr.query("select * from classes ", new MapListHandler());
+			System.out.println(list);
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
